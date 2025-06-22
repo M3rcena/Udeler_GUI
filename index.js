0c7d5dd95b0fc908e7ccef4730daf7cb57b18e16
M3rcena <102794864+M3rcena@users.noreply.github.com>
2025-06-22 21:18:42 +0300

    Refactor icon path in createWindow function and update package.json build configuration to include additional files and settings for Windows and NSIS installer.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let win;
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    icon: __dirname + "/assets/images/build/icon.png",
+    icon: path.join(__dirname, "assets/images/build/icon.png"),
     resizable: true,
     webPreferences: {
       nodeIntegration: true,
